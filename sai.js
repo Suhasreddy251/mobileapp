d3812b47d11515d2de562936c047c2c85de0d59f
sss <suhasreddy123@gmail.com>
2024-06-22 23:30:40 +0530

    master-01

---
+++ sai.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h2> JavaScript Main Page</h2>
+
+    <p id="main"> JavaScript Changes</p>
+    <button type="button" onClick='document.getElementById("main").innerHTML= "hey user welcome"'> submit </button>
+  </body>
+</html>
